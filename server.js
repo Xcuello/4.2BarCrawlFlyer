3dbc3fff1d8aec17b7a7eca72f693b7bb847ead8
Ryan <grossry@oregonstate.edu>
2019-02-18 12:32:47 -0500

    Changed directory in server.js

--- server.js
+++ server.js
@@ -7,17 +7,11 @@ const app = express();
 // Copied from: https://github.com/TutorialsOrg/angular-travis-ci-heroku
 /// Run the app by serving the static files
 // in the dist directory
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 
-// Start the app by listening on the default
-// Heroku port
+app.get('/*', function(req,res) {
 
-
-// // For all GET requests, send back index.html
-// // so that PathLocationStrategy can be used
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/flyer2/index.html'));
-  });
-
+    res.sendFile(path.join(__dirname+'/src/index.html'));
+    });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
