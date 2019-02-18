65f8a6d6abf5396144b628a136ebf8cf380dce23
Leon Hunter <leonhunter@Leons-MacBook-Pro-2.local>
2019-02-18 15:15:44 -0500

    new changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'flyer2'`, () => {
+  it(`should have as title '4.2 Bar Crawl Ticket'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('flyer2');
+    expect(app.title).toEqual('4.2 Bar Crawl Ticket');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to flyer2!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to 4.2 Bar Crawl Ticket!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '4.2BarCrawlFlyer';
+  title = '4.2 Bar Crawl Ticket';
 }
