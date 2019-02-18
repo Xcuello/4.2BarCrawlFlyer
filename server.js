b1744a4ca41c88129aff3d44211dc093eebc2a03
Ryan <grossry@oregonstate.edu>
2019-02-18 11:38:25 -0500

    Resolved directory in server.js

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const app = express();
 // Copied from: https://github.com/TutorialsOrg/angular-travis-ci-heroku
 /// Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/flyer2/index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + './dist/flyer2/index.html'));
   });
 
 // Start the app by listening on the default Heroku port
