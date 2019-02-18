ce33569529522551a38a177b3988704a420a5a53
Ryan <grossry@oregonstate.edu>
2019-02-18 11:26:55 -0500

    Reverted back changes in server.js, moved some dependencies from from devDependencies to dependencies

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname));
+// Copied from: https://github.com/TutorialsOrg/angular-travis-ci-heroku
+/// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname+'/src/index.html'));
-// });
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
