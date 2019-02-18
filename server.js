39ab650d3f4147000f2f3504b6e9f221c9bf33a3
Ryan <grossry@oregonstate.edu>
2019-02-18 12:46:41 -0500

    Changed directories in server.js
    No longer returns a 404 when viewing app, but is blank

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 // Copied from: https://github.com/TutorialsOrg/angular-travis-ci-heroku
 /// Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/src/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
     });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
